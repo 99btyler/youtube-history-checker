9d24dd8dbe0935afaf0d04fdf732527b122e2a4e
Tyler Beringer <99tylerberinger@gmail.com>
2021-04-12 13:03:20 -0400

    Pressed enter

--- youtube-history-checker/popup.js
+++ youtube-history-checker/popup.js
@@ -4,6 +4,7 @@ document.getElementById("search").onclick = function(element) {
 
 		document.getElementById("input").select();
 		document.execCommand("copy");
+		
 		window.open("https://www.youtube.com/feed/history");
 		
 	}
