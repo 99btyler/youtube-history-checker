90f811debf5bd6e6af126935bcffecd026e0c2fc
Tyler Beringer <99tylerberinger@gmail.com>
2021-05-28 17:37:55 -0400

    Removed confirm prompt

--- youtube-history-checker/src/popup.js
+++ youtube-history-checker/src/popup.js
@@ -1,12 +1,7 @@
 document.getElementById("search").onclick = function(element) {
 
-	if (confirm("Copy text to clipboard and open the \"search watch history\" page?")) {
-
-		document.getElementById("input").select();
-		document.execCommand("copy");
-		
-		window.open("https://www.youtube.com/feed/history");
-		
-	}
+	document.getElementById("input").select();
+	document.execCommand("copy");
+	window.open("https://www.youtube.com/feed/history");
 
 };
